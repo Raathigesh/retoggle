9b772b50ae6b092b40c9ebce76d9367dafb3168d
Raathigeshan <raathigesh92@gmail.com>
2018-11-04 23:50:09 +1100

    Delete test

--- src/lib/test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-
-const Container = styled.div``;
-
-export default function Hello() {
-  return <Container>Hello</Container>;
-}
