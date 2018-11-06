2641a16c5f4808ef86bdff534e3dda0bea78978b
Raathigeshan <raathigesh92@gmail.com>
2018-11-06 22:47:24 +1100

    Fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  preset: "ts-jest",
   testEnvironment: "jsdom",
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   transform: {
     "^.+\\.(ts|tsx)$": "babel-jest"
-  }
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
 };
