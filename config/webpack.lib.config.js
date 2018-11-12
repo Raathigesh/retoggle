51ce02e38dbe6c9697e065063d0d94c28f95ba1b
Raathigeshan <raathigesh92@gmail.com>
2018-11-12 21:15:39 +1100

    React as external

--- config/webpack.lib.config.js
+++ config/webpack.lib.config.js
@@ -32,5 +32,19 @@ module.exports = env => ({
         loader: "babel-loader"
       }
     ]
+  },
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    }
   }
 });
